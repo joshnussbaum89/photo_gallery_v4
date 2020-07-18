01dae73ea3e50ece69161ae4f74ff090810b46bb
Josh Nussbaum <Joshnussbaum89@gmail.com>
2020-07-17 22:58:23 -0400

    add JS search function

--- js/main.js
+++ js/main.js
@@ -2,5 +2,26 @@
 lightbox.option({
     fadeDuration: 300,
     wrapAround: true,
-    albumLabel: "%1 / %2"
+    albumLabel: "%1 / %2",
+    disableScrolling: true
 })
+
+//search items
+document.querySelector('#search').addEventListener('keyup', function () {
+
+    const input = document.querySelector('#search').value.toUpperCase();
+    const photos = document.querySelectorAll('.lightbox-parent a').length;
+
+    for (let i = 0; i < photos; i++) {
+
+        const description = document.querySelectorAll('a')[i].getAttribute('data-title').toUpperCase();
+
+        if (description.includes(input)) {
+            $(document.querySelectorAll('a')[i]).show();
+        }
+        else {
+            $(document.querySelectorAll('a')[i]).hide();
+        }
+    }
+
+});
