41667cbebb06a5ee5ec51ed6fcd569eb54ba63dc
Josh Nussbaum <Joshnussbaum89@gmail.com>
2020-09-20 15:19:32 -0400

    un comment js code

--- js/main.js
+++ js/main.js
@@ -27,32 +27,32 @@ document.querySelector('#search').addEventListener('keyup', function () {
     }
 });
 
-// $(function () {
-//     const availableTags = [
-//         "ActionScript",
-//         "AppleScript",
-//         "Asp",
-//         "BASIC",
-//         "C",
-//         "C++",
-//         "Clojure",
-//         "COBOL",
-//         "ColdFusion",
-//         "Erlang",
-//         "Fortran",
-//         "Groovy",
-//         "Haskell",
-//         "Java",
-//         "JavaScript",
-//         "Lisp",
-//         "Perl",
-//         "PHP",
-//         "Python",
-//         "Ruby",
-//         "Scala",
-//         "Scheme"
-//     ];
-//     $("#tags").autocomplete({
-//         source: availableTags
-//     });
-// });
+$(function () {
+    const availableTags = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+    ];
+    $("#tags").autocomplete({
+        source: availableTags
+    });
+});
